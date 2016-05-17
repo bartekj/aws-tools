ad8feb0e065f2b9feff0c1676c0fb2afec757708
Bart Jakubowski <bart.jakubowski@syncano.com>
2016-05-17 13:30:25 +0200

    Cleaning up

--- aws-env-update.py
+++ aws-env-update.py
@@ -8,8 +8,13 @@ import sys
 
 import gnupg
 
+debug = 0
+user = os.environ['USER']
+aws_config_dir = '/home/{0}/.aws/'.format(user)
+credential_file = '{0}/credentials'.format(aws_config_dir)
 
-def main():
+def get_args():
+    '''This function parses and return arguments passed in'''
     parser = argparse.ArgumentParser(__file__, formatter_class=argparse.RawDescriptionHelpFormatter,
                                     description=('''\
 Simple script that will pick up gpg encrypted files from ~/.aws
@@ -20,32 +25,34 @@ Example usage:\n\n\tx1:~$ awsenv test \n'''),
     parser.add_argument("-d", "--debug", action='store_true', help="Debug mode")
     parser.add_argument('-v', "--version", help="Print version", action='version', version='%(prog)s 1.0')
     args = parser.parse_args()
+    env = args.env
+    debug = args.debug
+    return env, debug
 
-    if args.debug:
+def main():
+    env, debug = get_args()
+
+    if debug:
         log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
         log.info("Debug output.")
     else:
         log.basicConfig(format="%(levelname)s: %(message)s")
 
-    ENV = args.env
-    USER = os.environ['USER']
-    AWS_CONFIG_DIR = '/home/{0}/.aws/'.format(USER)
-    ENCRYPTED_CREDENTIALS = glob.glob(AWS_CONFIG_DIR + "env." + ENV + ".conf.asc")
-    CREDENTIALS = '{0}/credentials'.format(AWS_CONFIG_DIR)
+    encrypted_credentials_file = os.path.join(os.sep, aws_config_dir, 'env.{0}.conf.asc'.format(env))
 
-    log.info('''Variables dump:\n\tENV : {0}\n\tUSER : {1}\n\tAWS_CONFIG_DIR : {2}\n\tENCRYPTED_CREDENTIALS : {3}
-    \tCREDENTIALS : {4}'''.format(ENV, USER, AWS_CONFIG_DIR, ENCRYPTED_CREDENTIALS, CREDENTIALS))
+    log.info('''Variables dump:\n\tenv : {0}\n\tuser : {1}\n\taws_config_dir : {2}\n\tencrypted_credentials_file : {3}
+    \tcredential_file : {4}'''.format(env, user, aws_config_dir, encrypted_credentials_file, credential_file))
 
-    if not ENCRYPTED_CREDENTIALS:
+    if not encrypted_credentials_file:
         log.error('''File with encrypted credentials for environment: {0} dont exists!.\n
-                 Please encrypt your aws keys to file: \n{1}env.{0}.conf.asc'''.format(ENV, AWS_CONFIG_DIR))
+                 Please encrypt your aws keys to file: \n{1}env.{0}.conf.asc'''.format(env, aws_config_dir))
 
-    gpg = gnupg.GPG(gnupghome='/home/{0}/.gnupg/'.format(USER))
+    gpg = gnupg.GPG(gnupghome='/home/{0}/.gnupg/'.format(user))
     private_keys = gpg.list_keys(True)
     if not private_keys:
         log.error('No private key(s) found! Please check your GPG config')
     phrase = getpass.getpass("Enter the passphrase to decrypt the env file: ")
-    stream = open(ENCRYPTED_CREDENTIALS[0], "rb")
+    stream = open(encrypted_credentials_file, "rb")
     output = gpg.decrypt_file(stream, passphrase=phrase)
     aws_credentials_patterns = ("aws_access_key_id", "aws_secret_access_key")
     if output.status == 'decryption failed':
@@ -53,12 +60,11 @@ Example usage:\n\n\tx1:~$ awsenv test \n'''),
     elif output.status == 'decryption ok':
         if any(x in str(output) for x in aws_credentials_patterns):
             try:
-                os.remove(CREDENTIALS)
+                os.remove(credential_file)
             except OSError:
                 pass
-            credentials_file = open(CREDENTIALS, 'w')
-            credentials_file.write(str(output))
-            credentials_file.close()
+            with open(credential_file, 'w') as outfile:
+                outfile.write(str(output))
         else:
             log.error('No AWS credentials in the decrypted file!')
     else:
