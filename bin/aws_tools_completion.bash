79b355890d0e12cd09727f15fb314b06a5f89434
Radar <radar@aol.pl>
2016-12-16 16:01:15 +0100

    migration to whl - first approach

---
+++ bin/aws_tools_completion.bash
@@ -0,0 +1,28 @@
+function __aws_env_update() {
+    eval $(aws-env-update.py $@)
+}
+
+function __aws_roll_keys() {
+    aws-roll-keys.py $@
+}
+
+function __awsenv_ps1() {
+    if [ -e "$HOME/.aws/credentials" ]; then
+        ps=$(cat $HOME/.aws/.env 2>/dev/null)
+    fi
+    if [ -n "$AWS_ENV" ]; then
+        ps="$AWS_ENV"
+    fi
+    echo "<$ps>"
+}
+
+function __aws_envs() {
+    COMPREPLY=()
+    local words=( "${COMP_WORDS[@]}" )
+    local word="${COMP_WORDS[COMP_CWORD]}"
+    words=("${words[@]:1}")
+    local completions="$(ls $HOME/.aws/env*asc | cut -d "." -f 3)"
+    COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+}
+
+complete -F __aws_envs awsenv
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='aws-tools',
+    version='1.0',
+    description='Switching between multiple AWS accounts and rolling keys',
+    url='https://github.com/bartekj/aws-tools',
+    scripts=[
+        'bin/aws-env-update.py',
+        'bin/aws-roll-keys.py',
+        'bin/aws_tools_completion.bash',
+    ],
+    include_package_data=True,
+    packages=find_packages('.', exclude=['tests*', 'docs*']),
+    install_requires=[
+        'argparse',
+        'boto3'
+        'python-gnupg>=0.3.9'
+    ],
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
