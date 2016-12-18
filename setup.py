47ba699a4e08355ff98764ee77f63b7f685c557f
Radar <radar@aol.pl>
2016-12-18 16:49:39 +0100

    setup.py - minor changes

--- setup.py
+++ setup.py
@@ -5,24 +5,27 @@ from setuptools import setup, find_packages
 setup(
     name='aws-tools',
     version='1.0',
-    description='Switching between multiple AWS accounts and rolling keys',
+    description='Switching between multiple AWS accounts & renewing API access keys',
+    license='MIT',
     url='https://github.com/bartekj/aws-tools',
+    keywords='aws key keys',
+    packages=find_packages(),
     scripts=[
         'bin/aws-env-update.py',
         'bin/aws-roll-keys.py',
         'bin/aws_tools_completion.bash',
     ],
-    include_package_data=True,
-    packages=find_packages('.', exclude=['tests*', 'docs*']),
     install_requires=[
         'argparse',
         'boto3',
         'python-gnupg>=0.3.9',
     ],
     classifiers=(
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console'
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
