b634a8ba82df8b735b1c49938feca0e9e1e2b97a
Kornik <radek.kornik@gmail.com>
2016-12-29 11:41:14 +0100

    Add python 3.5 compatibility

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ from setuptools import setup, find_packages
 
 try:
     version_git = subprocess.check_output(['git', 'describe', '--abbrev=0']).strip()
+    if type(version_git) is bytes:
+        version_git = version_git.decode('ascii')
 except:
     with open('version.py', 'r') as fh:
         version_git = open('version.py').read().split('=')[-1].strip()
+        if type(version_git) is bytes:
+            version_git = version_git.decode('ascii')
 
 with open('version.py', 'w') as fh:
     fh.write("# This file is managed by git\n" + "__version__ = " + version_git)
 
-with open('requirements.txt') as fh:
+with open('requirements.txt', 'r') as fh:
     required = fh.read().splitlines()
 
 setup(
@@ -30,7 +34,7 @@ setup(
         'bin/aws_tools_completion.bash',
     ],
     install_requires=required,
-    classifiers=(
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -40,5 +44,5 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-    ),
+    ],
 )
