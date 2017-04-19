ade279e1f77a6d5a3e3bd3507d5df94864cc0321
Radar <radar@aol.pl>
2017-04-19 09:30:47 +0200

    Python3.5 compatibilities

--- setup.py
+++ setup.py
@@ -4,24 +4,20 @@ import subprocess
 from setuptools import setup, find_packages
 
 try:
-    version_git = subprocess.check_output(['git', 'describe', '--abbrev=0']).strip()
-    if type(version_git) is bytes:
-        version_git = version_git.decode('ascii')
+    ver = subprocess.check_output(['git', 'describe', '--abbrev=0']).strip().encode()
 except:
     with open('version.py', 'r') as fh:
-        version_git = open('version.py').read().split('=')[-1].strip()
-        if type(version_git) is bytes:
-            version_git = version_git.decode('ascii')
+        ver = open('version.py').read().split('=')[-1].strip()
 
 with open('version.py', 'w') as fh:
-    fh.write("# This file is managed by git\n" + "__version__ = " + version_git)
+    fh.write('# This file is managed by git tag\n__version__ = {}\n'.format(ver))
 
 with open('requirements.txt', 'r') as fh:
     required = fh.read().splitlines()
 
 setup(
     name='aws-tools',
-    version=version_git,
+    version=ver,
     description='Switching between multiple AWS accounts & renewing API access keys',
     long_description=open('README.rst').read(),
     license='MIT',
@@ -42,7 +38,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
     ],
 )
