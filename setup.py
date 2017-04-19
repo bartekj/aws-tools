0d6c7e0409df6af68157d9b49932391308b6b500
Radar <radar@aol.pl>
2017-04-19 10:46:38 +0200

    Decode bytes type as utf-8

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools import setup, find_packages
 try:
     ver = subprocess.check_output(['git', 'describe', '--abbrev=0']).strip()
     if type(ver) is bytes:
-        ver = ver.encode()
+        ver = ver.decode('utf-8')
 except:
     with open('version.py', 'r') as fh:
         ver = open('version.py').read().split('=')[-1].strip()
         if type(ver) is bytes:
-            ver = ver.encode()
+            ver = ver.decode('utf-8')
 
 with open('version.py', 'w') as fh:
     fh.write('# This file is managed by git tag\n__version__ = {}\n'.format(ver))
